0f1b025f1ec70eda73089302eca4484ea78bd579
muawaz-dev <muawaz8@gmail.com>
2025-09-06 12:58:41 +0500

    Create model to enter bug information into the database

--- bugs/models.py
+++ bugs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+bug_choices=[
+    ('gameplay_or_mechanics','Gameplay & Mechanics'),
+    ('graphics_or_performance','Graphics & Performance'),
+    (' network_or_multiplayer',' Network & Multiplayer'),
+    ('ui_or_game_logic','UI & Game Logic'),
+    ('other','Other(Explain in Description)')
+]
+
 # Create your models here.
+class Bug(models.Model):
+    name=models.CharField( max_length=50)
+    bug_type=models.CharField(choices=bug_choices)
+    description=models.TextField()
+    number_of_occurences=models.IntegerField()
