2b6e80f0d02bdb6f0f1f4f3b2885958e88992bf7
muawaz-dev <muawaz8@gmail.com>
2025-09-07 06:51:33 +0500

    Create url and corresponding view to display/process form also create report.html to render the form

--- bugs/urls.py
+++ bugs/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('report/',views.report,name='report'),
+    path('success/',views.success,name='success'),
 ]
--- bugs/views.py
+++ bugs/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from . forms import BugForm
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def report(request):
+    if request.method == 'POST':
+        form=BugForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form=BugForm()
+    return render(request,'report.html',{'form':form})
+
+def success(request):
+    return render(request,'success.html')
